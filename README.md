b5323d506870a1d990ff0253c62f18cf58d0a689
Teacher-Venkatesh <12venkat21@gmail.com>
2020-02-20 18:34:13 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+#lab-form-validation
---
+++ script.js
@@ -0,0 +1,25 @@
+// Progression - 1 
+// Check username
+// create a function called as checkUserName()
+// username should not be empty
+// username should be a minimum of 8 characters and above 
+// username should be less than 15 characters
+// No space is allowed in username
+// username should not contain special characters
+// the function should return true if all the conditions above are validated.
+// return false if the validation fails
+
+// Progression - 2 
+// Check mail
+// create a function called as checkEmail()
+// only @ , . , _ [at,dot,underscore] is allowed
+// It can be alphanumeric in nature
+// return true if it passses all validation and false otherwise
+
+// Progression - 3
+// Check password
+// create a function called as checkPassword()
+// should be alphanumeric in nature
+// password must contain atleast one Uppercase, one number and special characters[!@#$%^&*()_]
+// return true else return false
+
